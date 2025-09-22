eb33e7a5c2b2fe14f6f6e4209877b4753440e690
Kelvin 2KAY Katoya <dambwedesigns@gmail.com>
2025-09-22 22:11:23 +0200

    i have added images corresponfing to each service in the service card adapting the design from the team's design approach in this way the services are eye catching and ensures a way more proffesional design

--- public/js/data-json.js
+++ public/js/data-json.js
@@ -1,20 +1,26 @@
 export const servicesSection = [
     {
+        serviceImage: 'app-development.png',
         h3Heading: 'Web Development',
         pContent: 'Custom websites tailored to your needs, built with modern technologies.'
     }, {
+        serviceImage: 'android-app-development.png',
         h3Heading: 'Mobile App Development',
         pContent: 'Intuitive apps for iOS and Android to engage your users on the go.'
     }, {
+        serviceImage: 'software-development.png',
         h3Heading: 'ICT Consultancy',
         pContent: 'Expert advice to optimize your tech infrastructure and strategies.'
     }, {
+        serviceImage: 'business-and-commerce.png',
         h3Heading: 'Student Project Assistance',
         pContent: 'Helping students with school projects and hands-on guidance.'
     }, {
+        serviceImage: 'software-developer.png',
         h3Heading: 'Software Training',
         pContent: 'Learn essential tools like Word, Excel, and other software suites.'
     }, {
+        serviceImage: 'infrastructure.png',
         h3Heading: 'Networking Skills',
         pContent: 'Training and solutions for robust networking setups.'
     },
--- public/js/services.js
+++ public/js/services.js
@@ -6,6 +6,7 @@ servicesSection.forEach((servicesCard) => {
     servicesSectionHTML += 
     `
     <div class="service-card">
+    <img src="/images/${servicesCard.serviceImage}" alt="service-image/icon" class="service-photo">
         <h3>${servicesCard.h3Heading}</h3>
             <p>${servicesCard.pContent}</p>
     </div>
